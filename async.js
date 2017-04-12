ddeb736c4e5320821aa689442ecdbb0e98c3c936
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-12 19:58:46 +0700

    init

---
+++ async.js
@@ -0,0 +1,9 @@
+let a = 5;let name = 'KHoa pham';
+
+setTimeout(function(){ a++; }, 1000);
+
+console.log(a);
+
+setTimeout(function(){ console.log(a) }, 1100);
+
+// setTimeout, doc ghi file, truy van database, request den service
---
+++ index.js
@@ -0,0 +1,11 @@
+const func = function(){
+    console.log('Xong')
+};
+
+setTimeout(func, 2000);
+setTimeout(func, 2000);
+
+let a = 5;
+a++;
+console.log(a);
+
